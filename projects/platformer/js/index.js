2662171535402682726c19c79660a37659059954
Daniel Aucoin <houdini10101@gmail.com>
2021-10-19 14:10:47 +0000

    add halle

--- projects/platformer/js/index.js
+++ projects/platformer/js/index.js
@@ -62,5 +62,7 @@ $(document).ready(function () {
         game.score.text = 'Score: ' + (parseInt(/\s+(\S*)$/.exec(game.score.text)[1], 10) + collectable.type.points);
         collectable.kill();
     }
+function drawImg(src,x,y,w,h){
 
+}
 });
