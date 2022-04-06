f71e778ead546fc8906edc585b436aac39beb7ac
Daniel Aucoin <houdini10101@gmail.com>
2022-04-06 14:17:12 +0000

    90 speed

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -25,7 +25,7 @@ var init = function (window) {
         // TODO 2 : Create a function that draws a circle 
         function drawCircle() {
             circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
-            physikz.addRandomVelocity(circle, canvas, 60, 60);
+            physikz.addRandomVelocity(circle, canvas, 90, 90);
             view.addChild(circle);
             circles.push(circle);
         }
